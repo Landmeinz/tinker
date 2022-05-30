5eb139eb56552b09e295e5135806965d5702edc4
Eric Meinzer <emeinzer7@gmail.com>
2022-05-30 13:10:20 -0500

    initial layout with some sample copy; placeholder photos

--- src/sxStyles.jsx
+++ src/sxStyles.jsx
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 export const theme = createTheme({
     palette: {
@@ -23,13 +23,16 @@ export const theme = createTheme({
     },
     breakpoints: {
         values: {
-            mobile: 0,
-            tablet: 640,
-            laptop: 1024,
-            desktop: 1280,
+            xs: 0,
+            sm: 640,
+            md: 960,
+            lg: 1280,
+            xl: 1920
         },
     },
 });
+
+// export const theme = responsiveFontSizes(themeStyle);
 
 export const sxApp = {
     bgcolor: 'info.light',
@@ -47,25 +50,28 @@ export const sxAppContainer = {
     height: '100vh',
     overflowY: 'scroll',
 
+
 }
 
-export const sxTopSection = {
-    border: '1px solid red',
+export const sxSectionOne = {
+    // border: '1px solid red',
     width: '100%',
     height: '100vh',
     display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: 'column',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    gap: 15,
 
 }
 
-export const   sxLeftColumn = {
-    border: '1px solid blue',
+export const sxLeftColumn = {
+    // border: '1px solid blue',
     width: '50%',
 
 }
 export const sxRightColumn = {
-    border: '1px solid green',
+    // border: '1px solid green',
     width: '50%',
 
 }
@@ -75,15 +81,93 @@ export const sxHeroTextContent = {
     wordWrap: 'break-word',
 
 }
-
 export const sxHeroText = {
     fontWeight: 'bold', // normal, bold, light
-    fontSize: '7.4rem',
     textAlign: 'center',
+
+    [theme.breakpoints.down('xl')]: {
+        fontSize: '7.4rem'
+    },
+    [theme.breakpoints.down('lg')]: {
+        fontSize: '6rem'
+    },
+    [theme.breakpoints.down('md')]: {
+        fontSize: '4rem'
+    },
+    [theme.breakpoints.down('sm')]: {
+        fontSize: '2.5rem'
+    }
 
 }
 
 
 export const sxHeroImageContent = {
+    // border: '1px solid red',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 25,
+
+}
+export const sxHeroImage = {
+    // border: '1px solid red',
+    height: '10rem',
+    width: '10rem',
+    borderRadius: '50%',
+
+}
+
+export const sxSectionTwo = {
+    // border: '1px solid blue',
+    mt: '20rem',
+    display: 'flex',
+    justifyContent: 'center',
     
 }
+
+export const sxSectionTwoContent = {
+    // border: '1px solid blue',
+    textAlign: 'center',
+    mx: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 5,
+
+
+}
+
+
+export const sxSectionThree = {
+    mt: '20rem',
+    display: 'flex',
+    justifyContent: 'center',
+    
+}
+
+export const sxSectionThreeContent = {
+    textAlign: 'center',
+    mx: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 5,
+
+}
+
+
+export const sxSectionFour = {
+    mt: '20rem',
+    display: 'flex',
+    justifyContent: 'center',
+    
+}
+
+export const sxSectionFourContent = {
+    textAlign: 'center',
+    mx: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 5,
+
+}
+
