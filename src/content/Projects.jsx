f35f9413d4e4494aa70f8ed2ee93487849a3595e
Eric Meinzer <emeinzer7@gmail.com>
2022-08-19 16:22:38 -0500

    resolved content

--- src/content/Projects.jsx
+++ src/content/Projects.jsx
@@ -102,85 +102,6 @@ export const swing = {
 // }
 
 
-<<<<<<< HEAD
-
-export const swing = {
-    title: 'New House, Old Tree, Two Swings For Newlyweds',
-    body: 'Owners of a new house were gifted with two handmade wooden teak swings hung and supported with 1" twisted manilla rope. Swings were designed in two collaboration sessions and were completely finished in three wood shop visits. ',
-    // details: 'noDetails',
-    images: [
-        {
-            id: 0,
-            path: '/images/swing/process1.jpg',
-            caption: 'sketches and tinkering',
-        },
-        {
-            id: 1,
-            path: '/images/swing/process2.jpg',
-            caption: 'teak wood breakdown',
-        },
-        {
-            id: 2,
-            path: '/images/swing/process3.jpg',
-            caption: 'making em smooth',
-        },
-        {
-            id: 3,
-            path: '/images/swing/finished1.jpg',
-            caption: 'linseed oil finish, ropes spliced in',
-        },
-        {
-            id: 4,
-            path: '/images/swing/finished2.jpg',
-            caption: 'finished product',
-        },
-        {
-            id: 5,
-            path: '/images/swing/finished3.jpg',
-            caption: 'swings installed',
-        }
-    ]
-}
-
-
-export const other = {
-    title: 'New Project Title Goes Here',
-    body: 'This is a longer description about this project',
-    // details: 'noDetails',
-    images: [
-        {
-            id: 0,
-            path: '/images/tools1.jpg',
-            caption: 'optional image caption',
-        },
-        {
-            id: 1,
-            path: '/images/tools1.jpg',
-            caption: 'imageCaption2',
-        },
-        {
-            id: 2,
-            path: '/images/tools1.jpg',
-            caption: 'imageCaption3',
-        },
-        {
-            id: 3,
-            path: '/images/tools1.jpg',
-            caption: 'imageCaption4',
-        },
-        {
-            id: 4,
-            path: '/images/tools1.jpg',
-            caption: 'imageCaption5',
-        },
-        {
-            id: 5,
-            path: '/images/tools1.jpg',
-            caption: 'imageCaption6',
-        }
-    ]
-}
-=======
 // export const other = {
 //     title: 'New Project Title Goes Here',
 //     body: 'This is a longer description about this project',
@@ -218,4 +139,3 @@ export const other = {
 //         }
 //     ]
 // }
->>>>>>> dc3ed6b0b7bf432a07befb9349cc54947dc40718
